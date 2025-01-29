6bc9e578a0c3da3efb628ce2025e7406bf3ba6e0
Gerald Hebert <54825353+ghebert3@users.noreply.github.com>
2025-01-28 19:22:35 -0600

    Hw1B

---
+++ Hw1B/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB);
+  angleMode(DEGREES)
+  noStroke();
+  
+}
+
+function draw() {
+   fill(0, 90, 100, 0.2); 
+  circle(710, 180, 200);
+
+ fill(200, 70, 200, 0.2); 
+  circle(620, 300, 200);
+  
+  fill(90,70,200, 0.2)
+  circle(800, 300, 200)
+  
+  
+
+}  
